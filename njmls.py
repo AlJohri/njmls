f25825b2e4baf0739828e2bf2a0c45da52bc0df6
Al Johri <al.johri@gmail.com>
2017-12-05 02:29:37 -0500

    get individual listing

--- njmls.py
+++ njmls.py
@@ -1,14 +1,75 @@
+import re
 import yaml
+import logging
 import requests
 import lxml.html
+from typing import List
 
 __version__ = '0.0.1'
 
-# response = requests.get("http://www.njmls.com/search.cfm")
-
 def get_listing_detail(mlsnum):
+    response = get_listing_detail_inner(mlsnum)
+    return parse_listing_detail(response)
+
+def get_listing_detail_inner(mlsnum):
     response = requests.get(f"http://www.njmls.com/listings/index.cfm?action=dsp.info&mlsnum={mlsnum}")
-    return response.text
+    if "Our web site is current down for maintenance" in response.text:
+        raise Exception("NJMLS is down for maintenance.")
+    return response
+
+def parse_listing_detail(response):
+
+    def get(listing, selector, name, fn):
+        try:
+            return [fn(x) for x in listing.cssselect(selector) if x.text == name][0]
+        except IndexError:
+            return None
+
+    def parse_str(x):
+        if x is not None:
+            return re.sub(r'\s+', ' ', x.strip())
+        return x
+
+    def parse_int(x):
+        if x is not None:
+            try:
+                return int(x.strip().replace(',', ''))
+            except ValueError:
+                pass
+        return x
+
+    def parse_money(x):
+        if x is not None:
+            return parse_int(x.strip().replace('$', '').replace(',', ''))
+        return x
+
+    doc = lxml.html.fromstring(response.text)
+    listing = doc.cssselect("#listsection table")[0]
+
+    """
+    #property1 > span > ul > li:nth-child(5) > a
+    <a href="/listings/index.cfm?action=xhr.map_get_directions&amp;mlsnum=1711398" class=" fancybox listing-map  { address: '52  Independence Dr', city: 'East Brunswick', state:'NJ',zip:'08816',mls_number: '1711398', lat: '40.414093', lng: '-74.435036',oh:false,status:'A' }" data-fancybox-type="iframe" "=""><div class="mapsResults" style="margin:5px">&nbsp;</div></a>
+
+    .mapsResults .getparent()
+    """
+
+    ret = {
+        'id': parse_int(get(listing, 'strong', 'MLS#:', lambda x: x).tail),
+        'address': parse_str(get(listing, 'strong', 'Address:', lambda x: x).tail),
+        'city': parse_str(get(listing, 'strong', 'Town:', lambda x: x).tail),
+        'price': parse_money(get(listing, 'strong', 'Current Price:', lambda x: x).tail),
+        'style': parse_str(get(listing, 'strong', 'Style:', lambda x: x).tail),
+        'rooms': parse_int(get(listing, 'strong', 'Rooms:', lambda x: x).tail),
+        'bedrooms': parse_int(get(listing, 'strong', 'Bedrooms:', lambda x: x).tail),
+        'baths_full': parse_int(get(listing, 'strong', 'Full Baths:', lambda x: x).tail),
+        'baths_part': parse_int(get(listing, 'strong', 'Half Baths:', lambda x: x).tail),
+        'basement': parse_str(get(listing, 'strong', 'Basement:', lambda x: x).tail).replace(',', ', '),
+        'garage': parse_str(get(listing, 'strong', 'Garage:', lambda x: x).tail).replace(',', ', '),
+        'list_date': parse_str(get(listing, 'strong', 'List Date:', lambda x: x).tail),
+        'tax': parse_money(get(listing, 'strong', 'Taxes:', lambda x: x).tail),
+    }
+
+    return ret
 
 # county search
 "http://www.njmls.com/listings/index.cfm?action=dsp.results&county=PASSAIC&state=NJ&proptype=1%2C3a%2C3b&searchtype=county_search&status=A"
@@ -71,9 +132,12 @@ def get_listings_inner(
     status='A',
     year_built="",
     sort_by="newest",
-    counties=[],
+    counties: List[str]=[],
     county_search='',
     proptypes=[]):
+
+    if isinstance(counties, str):
+        raise TypeError(f"counties must be a list, not a string")
 
     response = requests.get("http://www.njmls.com/listings/index.cfm", params={
         "zoomlevel": "0",
@@ -112,6 +176,9 @@ def get_listings_inner(
         "emailalert_yn": "N", # what is this?
     })
 
+    if "Our web site is current down for maintenance" in response.text:
+        raise Exception("NJMLS is down for maintenance.")
+
     return response
 
 def parse_row(row):
@@ -119,21 +186,29 @@ def parse_row(row):
     # Yaml requres a space after the colon
     # http://pyyaml.org/wiki/YAMLColonInFlowContext
     data_str = data_str.replace(":'", ": '")
-    data = yaml.load(data_str)
-    data.pop('oh') # don't care about open house temporarily
-    data.pop('detailurl') # don't care about detailurl for now, mls_number should be enough
+    listing = yaml.load(data_str)
+    listing.pop('oh') # don't care about open house temporarily
+    listing.pop('detailurl') # don't care about detailurl for now, mls_number should be enough
+    listing.pop('state') # state is redundant for now
     # /listings/index.cfm?action=dsp.info&mlsnum=1746570&dayssince=15&countysearch=true
-    return data
+    listing['price'] = int(listing['price'].replace('$', '').replace(',', ''))
+    listing['id'] = listing.pop('mls_number')
+    listing['bedrooms'] = listing.pop('beds')
+    listing['address'] = listing['address'].replace('  ', ' ')
+    return listing
 
 def get_listings(**kwargs):
 
     response = get_listings_inner(page=1, **kwargs)
     doc = lxml.html.fromstring(response.text)
-    current_page = int([x.text for x in doc.cssselect('#pagelist2 ol.pagenumbers li:not(.currentPage)') if not x.get('a')][0])
+    try:
+        current_page = int([x.text for x in doc.cssselect('#pagelist2 ol.pagenumbers li:not(.currentPage)') if not x.get('a')][0])
+    except Exception as e:
+        import pdb; pdb.set_trace()
     last_page = int(doc.cssselect('#pagelist2 ol.pagenumbers li.currentPage:last-child')[0].text.replace('of ', ''))
     result_count = int(doc.cssselect("#resultcount")[0].text)
-    # if result_count == 500:
-    #     logging.warning("only returning first 500 results")
+    if result_count == 500:
+        logging.warning(f"filters too broad. only returning first 500 results. filters: {str(kwargs)}")
     # print("page", current_page, "of", last_page)
     # print("result count", )
     yield from [parse_row(x) for x in doc.cssselect("#list-view-wrap .houseresults")]
